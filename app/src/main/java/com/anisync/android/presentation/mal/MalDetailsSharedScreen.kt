f81a913dde3eef8d9a420fa65c23488b0b6d6a18
nixjoyer <Error5634@proton.me>
2026-07-24 18:29:41 +0200

    fix(details): import scaffold content padding

--- app/src/main/java/com/anisync/android/presentation/mal/MalDetailsSharedScreen.kt
+++ app/src/main/java/com/anisync/android/presentation/mal/MalDetailsSharedScreen.kt
@@ -1,5 +1,6 @@
 package com.anisync.android.presentation.mal
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
