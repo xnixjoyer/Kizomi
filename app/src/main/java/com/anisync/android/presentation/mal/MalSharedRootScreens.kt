74b7f7dc80a88786597e1057bad643cfaa79ef49
nixjoyer <Error5634@proton.me>
2026-07-24 15:40:34 +0200

    fix(mal): use saveable state import in shared roots

--- app/src/main/java/com/anisync/android/presentation/mal/MalSharedRootScreens.kt
+++ app/src/main/java/com/anisync/android/presentation/mal/MalSharedRootScreens.kt
@@ -24,7 +24,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
